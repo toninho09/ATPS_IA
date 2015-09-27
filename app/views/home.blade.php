c26620b8141c2e69cc7e8145a71d3d252f329329
Antonio <toninho0007@yahoo.com.br>
2015-09-27 14:16:09 -0300

    nome do grupo
    nome do grupo na tela inicial

--- app/views/home.blade.php
+++ app/views/home.blade.php
@@ -12,3 +12,11 @@
 {{Form::select('destino', $cidades)}}<br>
 <button id="cadastrar">Ver Ligações</button>
 </form>
+
+<hr>
+<br><br>
+<label>Grupo do trabalho:</label><br>
+<label>Antonio Henrique Lorenzon dos Santos - 6814016110</label><br>
+<label>Breno Vicentim - 7086561978</label><br>
+<label>Renato de Souza - 7476682279</label><br>
+<label>Richard Donizete - 6814004983</label><br>
